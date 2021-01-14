12d454e0dfee9f2c1780e1887dd7dc6dd8297029
plutospp <davidchen741107@gmail.com>
2021-01-14 17:36:10 +0800

    Update trainval.py

--- trainval.py
+++ trainval.py
@@ -1,5 +1,6 @@
 import segmentation_models as sm
 from data_generator import DataGenerator
+from clr_callback import CyclicLR
 
 BACKBONE = 'vgg19shrink'
 preprocess_input = sm.get_preprocessing(BACKBONE)
@@ -24,7 +25,15 @@ model = sm.Unet(BACKBONE, encoder_weights='imagenet')
 model.compile(
     'SGD',
     loss=sm.losses.binary_crossentropy,
-    metrics=[sm.metrics.seg_recall, sm.metrics.seg_precision],
+    metrics=[sm.metrics.recall, sm.metrics.precision],
+    callbacks=[
+        CyclicLR(
+	        mode=config.CLR_METHOD,
+	        base_lr=config.MIN_LR,
+	        max_lr=config.MAX_LR,
+	        step_size=config.STEP_SIZE*(trainX.shape[0]//config.BATCH_SIZE)
+        )
+    ]
 )
 
 # fit model
